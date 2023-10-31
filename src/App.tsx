6ec336a0b0a9a19ed6950b8d64cbd993dd7c4931
sheryarj <sheryar.janjua@hotmail.com>
2023-10-31 04:42:45 +0000

    add in hook for data

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,28 @@ import { useRef, useState, Children } from "react";
 import { easeIn, easeOut } from "polished";
 import { useBoolean } from "react-use";
 import { createReducer } from "@reduxjs/toolkit";
+import { useMovies } from "./hooks";
 
 // TODO: use https://giddy-beret-cod.cyclic.app/movieCompanies
 const mockMovieCompanyData: any = [{ id: "1", name: "Test Productions" }];
 
-// TODO: use https://giddy-beret-cod.cyclic.app/movies
-const mockMovieData: any = [
-  {
-    id: "1",
-    reviews: [6, 8, 3, 9, 8, 7, 8],
-    title: "A Testing Film",
-    filmCompanyId: "1",
-    cost: 534,
-    releaseYear: 2005,
-  },
-  {
-    id: "2",
-    reviews: [5, 7, 3, 4, 1, 6, 3],
-    title: "Mock Test Film",
-    filmCompanyId: "1",
-    cost: 6234,
-    releaseYear: 2006,
-  },
-];
-
 export const App = () => {
-  const movieLength = useRef(mockMovieData.length);
-  const [selectedMovie, setSelectedMovie] = useState(0);
+  const { data, error, loading } = useMovies();
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  console.log(data);
+
+  const movieLength = data.length; //use ref here
+
+  // selected movie logic
+  // const [selectedMovie, setSelectedMovie] = useState({});
 
   const refreshButton = (buttonText: any) => {
     if (mockMovieCompanyData) {
@@ -42,13 +37,13 @@ export const App = () => {
     <div>
       <h2>Welcome to Movie database!</h2>
       {refreshButton("Refresh")}
-      <p>Total movies displayed {movieLength.current}</p>
+      <p>Total movies displayed {movieLength}</p>
       <span>Title - Review - Film Company</span>
       <br />
-      {mockMovieData.map((movie: any) => (
+      {data.map((movie: any) => (
         <span
           onClick={() => {
-            setSelectedMovie(movie);
+            console.log(movie);
           }}
         >
           {movie.title}{" "}
@@ -65,7 +60,7 @@ export const App = () => {
       ))}
       <br />
       <div>
-        {selectedMovie
+        {/* {selectedMovie
           ? (selectedMovie.title as any)
             ? (("You have selected " + selectedMovie.title) as any)
             : "No Movie Title"
@@ -78,7 +73,7 @@ export const App = () => {
               <input type="text" />
             </label>
           </form>
-        )}
+        )} */}
       </div>
     </div>
   );
