8ab56b61b867e669c5ee8ca505b84f729586da00
sheryarj <sheryar.janjua@hotmail.com>
2023-11-10 16:00:46 +0000

    update movie table

--- src/components/MovieTable.tsx
+++ src/components/MovieTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,14 +7,36 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
+import { sortMovies } from "../helpers";
+import Switch from "@mui/material/Switch";
+import { MovieInfo } from "../types";
 
-export const MovieTable = ({ movieData }: any) => {
-  const HeaderCell = ({ title }: any) => {
+export const MovieTable = ({
+  movieData,
+  selectedMovie,
+  setSelectedMovie,
+}: {
+  movieData: MovieInfo | any;
+  selectedMovie: MovieInfo;
+  setSelectedMovie: any;
+}): JSX.Element => {
+  const [sortType, setSortType] = useState(true);
+  const [sortedData, setSortedData] = useState(sortMovies(movieData, "desc"));
+
+  const HeaderCell = ({ title }: { title: string }) => {
     return <TableCell sx={{ color: "#FFF", fontWeight: "bold", fontSize: 20 }}>{title}</TableCell>;
   };
 
   return (
     <Container>
+      Ascending order
+      <Switch
+        onChange={() => {
+          setSortType(!sortType);
+          setSortedData(sortMovies(movieData, sortType ? "asc" : "desc"));
+        }}
+      />
+      descending order
       <TableContainer component={Paper} sx={{ borderRadius: "15px", bgcolor: "#1E1E1E" }}>
         <Table sx={{ minWidth: 650, bgcolor: "#1E1E1E", color: "#FFF" }}>
           <TableHead>
@@ -25,16 +47,22 @@ export const MovieTable = ({ movieData }: any) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {movieData.map((row: any) => (
+            {sortedData.map((row: any) => (
               <TableRow
+                onClick={() => {
+                  setSelectedMovie(row);
+                }}
                 key={row.title}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 }, bgcolor: "#1h1h1h" }}
+                sx={{
+                  "&:last-child td, &:last-child th": { border: 0 },
+                  bgcolor: selectedMovie?.movieTitle === row.movieTitle ? "#101418" : "#1h1h1h",
+                }}
               >
                 <TableCell sx={{ color: "#FFF" }} component="th" scope="row">
-                  {row.title}
+                  {row.movieTitle}
                 </TableCell>
-                <TableCell sx={{ color: "#FFF" }}>{row.rating}</TableCell>
-                <TableCell sx={{ color: "#FFF" }}>{row.filmCompanyId}</TableCell>
+                <TableCell sx={{ color: "#FFF" }}>{row.reviews}</TableCell>
+                <TableCell sx={{ color: "#FFF" }}>{row.filmCompanyName}</TableCell>
               </TableRow>
             ))}
           </TableBody>
